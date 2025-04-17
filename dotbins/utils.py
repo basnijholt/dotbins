c8eb990583710655dfce7f18a42df257d9c182f1
Bas Nijholt <bas@nijho.lt>
2025-04-17 11:24:54 -0700

    Fix nushell on Windows

--- dotbins/utils.py
+++ dotbins/utils.py
@@ -183,15 +183,16 @@ def _format_shell_instructions(
         return base_script
 
     if shell == "nushell":
+        tools_dir_str_nu = tools_dir_str.replace("$HOME", "($nu.home-path)")
         script_lines = [
             "# dotbins - Add platform-specific binaries to PATH",
-            "let _os = (sys).host.name | str downcase",
+            "let _os = sys host | get name | str downcase",
             'let _os = if $_os == "darwin" { "macos" } else { $_os }',
             "",
-            "let _arch = (sys).host.arch",
+            "let _arch = sys host | get arch",
             'let _arch = if $_arch == "x86_64" { "amd64" } else if $_arch in ["aarch64", "arm64"] { "arm64" } else { $_arch }',
             "",
-            f'$env.PATH = ($env.PATH | prepend $"{tools_dir_str}/$_os/$_arch/bin")',
+            f'$env.PATH = ($env.PATH | prepend $"{tools_dir_str_nu}/($_os)/($_arch)/bin")',
         ]
         base_script = "\n".join(script_lines)
         if_start = "if (which {name}) != null {{"
